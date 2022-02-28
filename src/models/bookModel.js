823c7148578c04455121e9ca09b1fa0119b77124
Hqsqae20 <swapna.d9484@gmail.com>
2022-03-01 03:51:05 +0530

    bookschema

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -3,29 +3,18 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema( {
     bookName: String, 
     authorName: String, 
+    year:Number,
     tags: [String],
-    
-    isPublished: Boolean,
+    totalPages :Number,
+    stockAvailable :Boolean,
+
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book', bookSchema) 
 
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
